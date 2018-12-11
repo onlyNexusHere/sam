f09336edd946c4a7c1b7d0e415ec11973b48570d
Line-Yin <line2860@gmail.com>
2018-12-10 20:37:37 -0500

    update vision

--- detect_rgb.py
+++ detect_rgb.py
@@ -1,7 +1,5 @@
 import numpy as np
 import cv2
-from PIL import Image
-import time
 
 
 def detect(img):
@@ -10,14 +8,17 @@ def detect(img):
     img = cv2.GaussianBlur(img, (5, 5), 0)
     hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
 
-    yellow_low = np.array([26, 57, 46])
+    # latest
+    yellow_low = np.array([26, 30, 46])
     yellow_high = np.array([34, 255, 255])
 
+    # latest
     white_low = np.array([0, 0, 200])
-    white_high = np.array([180, 60, 255])
+    white_high = np.array([180, 30, 255])
 
-    red_low = np.array([170, 90, 0])
-    red_high = np.array([180, 255, 255])
+    # latest
+    red_low = np.array([0, 90, 0])
+    red_high = np.array([10, 255, 255])
 
     mask_white = cv2.inRange(hsv_img, white_low, white_high)
     mask_yellow = cv2.inRange(hsv_img, yellow_low, yellow_high)
@@ -27,9 +28,11 @@ def detect(img):
     w_ratio = float(format((mask_white > 0).sum() / float(w*h), '.4f'))
     r_ratio = float(format((mask_red > 0).sum() / float(w*h), '.4f'))
 
-    y_thresh = float(0.005)
-    w_thresh = float(0.044)
-    r_thresh = float(0.18)
+    y_thresh = float(0.01)
+    w_thresh = float(0.02)
+    r_thresh = float(0.25)
+
+    print(y_ratio, w_ratio, y_ratio)
 
     if r_ratio >= r_thresh:
         command = 'stop'
@@ -37,8 +40,12 @@ def detect(img):
 
     elif y_ratio < y_thresh:
         white_pix_r, white_pix_c = np.where(mask_white > 0)
-        mid = [int(np.median(white_pix_r)), int(np.median(white_pix_c)/2)]
-        command = 'turn left'
+        if len(white_pix_r) == 0 or len(white_pix_c) == 0:
+            mid = [int(w/2), int(w/2)]
+            command = 'straight'
+        else:
+            mid = [int(np.median(white_pix_r)), int(np.median(white_pix_c)/2)]
+            command = 'turn left'
 
     elif y_ratio >= y_thresh and w_ratio < w_thresh:
         yellow_pix_r, yellow_pix_c = np.where(mask_yellow > 0)
@@ -59,13 +66,13 @@ def detect(img):
     else:
         mid = [-1, -1]
 
-    return int(w/2), mid, command
+    return int(w/2), mid[1], command, [y_ratio, w_ratio, r_ratio]
 
 
-for i in range(9):
-    img = np.array(Image.open('1028/test_img/path_' + str(i) + '.jpg'))
-    s = time.time()
-    center, mid, command = detect(img)
-    print(' ')
-    print(center, mid, command)
-    print(time.time() - s)
+# for i in range(9):
+#     img = np.array(Image.open('1028/test_img/path_' + str(i) + '.jpg'))
+#     s = time.time()
+#     center, mid, command = detect(img)
+#     print(' ')
+#     print(center, mid, command)
+#     print(time.time() - s)
--- test_vision.py
+++ test_vision.py
@@ -2,8 +2,10 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
-from detect_rgb2 import detect
+from detect_rgb3 import detect
 import serial
+import numpy as np
+import os
 
 
 port = '/dev/ttyACM0'
@@ -15,40 +17,117 @@ Y_RESOLUTION = 960
 # Initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (X_RESOLUTION, Y_RESOLUTION)
-camera.framerate = 10
+camera.framerate = 5
 rawCapture = PiRGBArray(camera, size=(X_RESOLUTION, Y_RESOLUTION))
 
 # Allow camera to warmup
 time.sleep(0.1)
 i = 0
-#Capture frames from the camera
+
+
+def speed(left, right):
+    motor_control = "m " + str(int(left)) + " " + str(int(right))
+    ser.write(motor_control.encode())
+
+
+test = 0
+folder = 'test_' + str(test)
+if not os.path.isdir(folder):
+    os.mkdir(folder)
+
 for frame in camera.capture_continuous(rawCapture, format="rgb", use_video_port=True):
     # Grab the raw NumPy array representing the image
-    start = time.time()
     img = frame.array
     key = cv2.waitKey(1) & 0xFF
+
+    # cv2.imwrite('0.png', img)
+
+
     # Clear the stream so it is ready to receive the next frame
     rawCapture.truncate(0)
-    center, mid, command = detect(img)
 
-    if command == 'straight':
+    if i != 0:
+        print('= = = = =')
+        print('frame: ' + str(i))
 
-        print(":" + command + ":")
-        # motor_control = str(1) + ' ' + str(150) + ' ' + str(150) + ' '
-        motor_control = "m 150 150"
-        ser.write(motor_control.encode())
+        center, mid, command, ratio = detect(img)
 
+        print(command, ratio)
+        print([center, mid])
 
+        sta = [command, ratio, center, mid]
+        # cv2.imwrite(folder + '/' + str(i) + '_' + str(sta) + '.png', img[int(img.shape[0] / 2):int(img.shape[0]), :, :])
 
-        # print(ser.readline().decode('utf-8'))
-    else:
-        print(":" + command + ":")
-        motor_control = "m 140 215"
-        ser.write(motor_control.encode())
-        # print(ser.readline().decode('utf-8'))
-    # print('frame: ' + str(i))
-    # print(command + ' ' + str([center, mid[1]]))
-    end = time.time()
-    # print('time: ' + str(end - start))
+        ml = mr = 150
+
+        diff = center - mid
+
+        # rat = np.abs(diff) / (center / 2)
+
+        change_left = 25
+        change_right = 27
+
+        stay = 100
+
+        # if command == 'stop':
+        #     print('Stop')
+        #     speed(0, 0)
+
+        if np.abs(diff) <= stay:
+            print('Stay on center')
+            if diff > 0:
+                speed(ml-1, mr + 2)
+            else:
+                speed(ml + 2, mr-1)
+
+        elif diff > stay:
+            print('Turn left')
+            speed(ml - 30, mr + 35)
+
+        elif diff < -stay:
+            print('Turn right')
+            speed(ml+35, mr-30)
+
     i += 1
+
+
+
+
+    # if command == 'straight':
+    #
+    #     if 0 < center < mid[1] - 160 * 2:
+    #         motor_control = "m 151 150"
+    #         ser.write(motor_control.encode())
+    #     elif mid[1] + 160 * 2 < center < mid[1] * 2:
+    #         motor_control = "m 150 151"
+    #         ser.write(motor_control.encode())
+    #     else:
+    #         motor_control = "m 150 150"
+    #         ser.write(motor_control.encode())
+    #
+    # elif command == 'turn left':
+    #     print(":" + command + ":")
+    #     motor_control = "m 0 0"
+    #     ser.write(motor_control.encode())
+    #     break
+    #
+    # elif command == 'turn right':
+    #     print(":" + command + ":")
+    #     motor_control = "m 0 0"
+    #     ser.write(motor_control.encode())
+    #     break
+    #
+    # else:
+    #     break
+    #     print(":" + command + ":")
+    #     motor_control = "m 0 0"
+    #     ser.write(motor_control.encode())
+
+    # if i == 30:
+    #     break
+
+    # i += 1
+
+# print(yellow)
+# print(white)
 
