c906db651deef63ff816b1eb7913cb028a3b3adb
onlyNexusHere <jktangen@gmail.com>
2018-12-16 15:49:18 -0500

    moving items around to clean up the main folder

--- test_vision_socket.py
+++ test_vision_socket.py
@@ -2,7 +2,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
-from detect_rgb5 import detect
+from support.detect_rgb5 import detect
 import serial
 import numpy as np
 import os
